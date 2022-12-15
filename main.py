d19b84b839146e7c8991ec828c00c02cd882da2e
k21066-Shimizu <k21066kk@aitech.ac.jp>
2022-12-15 15:04:12 +0900

    クエリパラメータで表示する写真を切り替えるよう変更

--- main.py
+++ main.py
@@ -1,17 +1,24 @@
-from flask import Flask, request, render_template, jsonify
+from flask import Flask, request, render_template, redirect
 import os
 
 app = Flask(__name__)
 app.config["JSON_AS_ASCII"] = False  # 日本語などのASCII以外の文字列を返したい場合は、こちらを設定しておく
 
-UPLOAD_PATH = './static/images/upload'
-
 # http://127.0.0.1:5000/address
 @app.route('/display-images', methods=["GET"])
 def address_get():
-    files = [os.path.join(UPLOAD_PATH, name) for name in os.listdir(UPLOAD_PATH)]
-    print(files)
-    return render_template("display-images.html", images=files)
+    type = request.args.get('type', None)
+    if type == 'upload':
+        folder_path = './static/images/upload'
+        type_jp = 'アップロード画像'
+    elif type == 'grayscale':
+        folder_path = './static/images/grayscale'
+        type_jp = 'グレースケール画像'
+    else:
+        return redirect('/display-images?type=upload')
+
+    files = [os.path.join(folder_path, name) for name in os.listdir(folder_path)]
+    return render_template("display-images.html", images=files, type_jp=type_jp)
 
 # http://127.0.0.1:5000/
 @app.route('/')
